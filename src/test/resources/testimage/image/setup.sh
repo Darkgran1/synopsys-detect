95847a96c67d44ab2ca872872c04229fac9498a8
Steve Billings <sbillings@blackducksoftware.com>
2019-03-11 19:02:45 -0400

    add dotnet

--- src/test/resources/testimage/image/setup.sh
+++ src/test/resources/testimage/image/setup.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
+# Install dotnet
+apt-get install -y software-properties-common
+add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+apt-get -y update
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get install -y apt-transport-https
+apt-get -y update
+apt-get install -y libicu55
+apt-get install -y dotnet-runtime-deps-2.2
+apt-get install -y aspnetcore-runtime-2.2 dotnet-runtime-2.2
+apt-get install -y dotnet-sdk-2.2
+
 cd /opt/blackduck/detect
 rm -rf detect.sh
 
 # get detect
 curl -O https://detect.synopsys.com/detect.sh
 chmod +x detect.sh
+
+# Create dotnet sample project
+cd testprojects
+mkdir dotnet-hello-world
+cd dotnet-hello-world
+dotnet new console
+
