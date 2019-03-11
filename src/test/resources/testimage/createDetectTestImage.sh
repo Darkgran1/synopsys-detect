ff564dd6a8003a979c418eb24e48994e42cad32b
Steve Billings <sbillings@blackducksoftware.com>
2019-03-11 17:14:27 -0400

    curl down the other files that it needs

--- src/test/resources/testimage/createDetectTestImage.sh
+++ src/test/resources/testimage/createDetectTestImage.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Run this from an empty directory from a Linux machine that:
+# - Has Docker installed
+# - Has git installed
+# - Is configured with a certificate for accessing github
 
 imageorg=blackducksoftware
 imagename=detecttest
@@ -10,7 +15,12 @@ docker stop ${containername} || true
 docker rm ${containername} || true
 docker rmi ${imageorg}/${imagename}:${imageversion} || true
 
+rm -rf image
+mkdir image
 cd image
+curl -O https://raw.githubusercontent.com/blackducksoftware/synopsys-detect/sb_testContainer/src/test/resources/testimage/image/Dockerfile
+curl -O https://raw.githubusercontent.com/blackducksoftware/synopsys-detect/sb_testContainer/src/test/resources/testimage/image/setup.sh
+chmod a+x setup.sh
 
 # Get blackduck-common
 rm -rf blackduck-common
@@ -28,9 +38,10 @@ rm -rf blackduck-common
 rm -rf common-maven-parent
 
 echo "Next steps:"
-echo "docker run -it -d --name ${containername} ${imageorg}/${imagename}:${imageversion}"
-echo "docker attach ${containername}"
+echo "docker run -it -d --name ${containername} ${imageorg}/${imagename}:${imageversion}; docker attach ${containername}"
 echo "cd /opt/blackduck/detect/"
 echo "./setup.sh"
 echo "cd testprojects/blackduck-common/"
 echo "../../detect.sh --blackduck.url=https://yourblackduckserver.yourdomain --blackduck.username=yourusername --blackduck.password=yourpassword --blackduck.trust.cert=true"
+echo "cd ../common-maven-parent"
+echo "<re-run the detect.sh command>"
