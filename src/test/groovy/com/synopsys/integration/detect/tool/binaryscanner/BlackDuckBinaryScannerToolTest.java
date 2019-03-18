ad662b205491201480d56121095c339bf4d10eea
Steve Billings <sbillings@blackducksoftware.com>
2019-03-18 17:10:08 -0400

    refactoring

--- src/test/groovy/com/synopsys/integration/detect/tool/binaryscanner/BlackDuckBinaryScannerToolTest.java
+++ src/test/groovy/com/synopsys/integration/detect/tool/binaryscanner/BlackDuckBinaryScannerToolTest.java
@@ -19,9 +19,11 @@ public class BlackDuckBinaryScannerToolTest {
     public void testShouldRunFalseNonExistent() {
         final DetectConfiguration detectConfiguration = Mockito.mock(DetectConfiguration.class);
         Mockito.when(detectConfiguration.getProperty(DetectProperty.DETECT_BINARY_SCAN_FILE, PropertyAuthority.None)).thenReturn("thisisnotafile");
+        
         final BlackDuckBinaryScannerTool tool = new BlackDuckBinaryScannerTool(null,
             null, detectConfiguration, null);
         boolean shouldRunResponse = tool.shouldRun();
+
         assertFalse(shouldRunResponse);
     }
 
@@ -30,15 +32,16 @@ public class BlackDuckBinaryScannerToolTest {
     public void testShouldRunFalseDirectory() {
         final DetectConfiguration detectConfiguration = Mockito.mock(DetectConfiguration.class);
         Mockito.when(detectConfiguration.getProperty(DetectProperty.DETECT_BINARY_SCAN_FILE, PropertyAuthority.None)).thenReturn(".");
+
         final BlackDuckBinaryScannerTool tool = new BlackDuckBinaryScannerTool(null,
             null, detectConfiguration, null);
         boolean shouldRunResponse = tool.shouldRun();
+
         assertFalse(shouldRunResponse);
     }
 
     @Test
     public void testShouldRunTrue() throws IOException {
-
         final File binaryScanFile = Files.createTempFile("test", "binaryScanFile").toFile();
         binaryScanFile.deleteOnExit();
         assertTrue(binaryScanFile.canRead());
@@ -46,9 +49,11 @@ public class BlackDuckBinaryScannerToolTest {
 
         final DetectConfiguration detectConfiguration = Mockito.mock(DetectConfiguration.class);
         Mockito.when(detectConfiguration.getProperty(DetectProperty.DETECT_BINARY_SCAN_FILE, PropertyAuthority.None)).thenReturn(binaryScanFile.getAbsolutePath());
+
         final BlackDuckBinaryScannerTool tool = new BlackDuckBinaryScannerTool(null,
             null, detectConfiguration, null);
         boolean shouldRunResponse = tool.shouldRun();
+
         assertTrue(shouldRunResponse);
     }
 }
